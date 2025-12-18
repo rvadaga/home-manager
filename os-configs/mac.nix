6e65984af2d7dc1351cb76cf340f35f8fd3e1cd3
Rahul Vadaga <rvadaga@etsy.com>
2025-12-18 15:24:02 -0500

    fix coreutils collision by replacing coreutils-prefixed with custom g-prefixed symlinks

--- os-configs/mac.nix
+++ os-configs/mac.nix
@@ -6,7 +6,14 @@
   home = {
     packages = [
       pkgs.unstable.coreutils-full
-      pkgs.unstable.coreutils-prefixed
+      # g-prefixed symlinks for gnu coreutils (avoids collision with coreutils-prefixed)
+      (pkgs.runCommand "coreutils-gprefixed" {} ''
+        mkdir -p $out/bin
+        for cmd in ${pkgs.unstable.coreutils-full}/bin/*; do
+          name=$(basename "$cmd")
+          ln -s "$cmd" "$out/bin/g$name"
+        done
+      '')
       pkgs.pinentry_mac
     ];
   };
