0f7c00a165a0dc1a10df9cadeb6ebdf51909e953
Rahul Vadaga <rvadaga@etsy.com>
2026-04-24 16:52:51 -0400

    scope IdentitiesOnly to github.com (fixes step ssh cert auth)

--- os-configs/mac.nix
+++ os-configs/mac.nix
@@ -32,13 +32,15 @@
       "*" = {
         extraOptions = {
           AddKeysToAgent = "yes";
-          IdentitiesOnly = "yes";
         };
       };
       "github.com" = {
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_ed25519";
+        extraOptions = {
+          IdentitiesOnly = "yes";
+        };
       };
     };
     includes = [
