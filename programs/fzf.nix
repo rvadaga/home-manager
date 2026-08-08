f26d8350186d359db7879c8159b12e48fecf6f70
Rahul Vadaga <rvadaga@etsy.com>
2026-08-08 17:48:30 -0400

    guard fzf zsh integration in non-tty shells

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -1,5 +1,13 @@
-{ config, pkgs, ... }:{
+{ config, ... }: {
   programs.fzf = {
     enable = true;
+    enableZshIntegration = false;
   };
+
+  programs.zsh.initContent = ''
+    # fzf's widgets need a terminal because their setup restores the zle option.
+    if [[ -o zle && -t 0 ]]; then
+      source <(${config.programs.fzf.package}/bin/fzf --zsh)
+    fi
+  '';
 }
